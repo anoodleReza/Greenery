12130f35d1c819666de4e230b4c196a3b9995520
muhammad zaky <muhammad.zaky.firdaus@mail.ugm.ac.id>
2023-03-23 09:26:17 +0700

    menu thingy

---
+++ screens/merchant/MerchantAddMenu.tsx
@@ -0,0 +1,423 @@
+//building the screen
+import {Button, SegmentedButtons} from 'react-native-paper';
+import {Text, View, TextInput, Image, Platform, Alert} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StackActions} from '@react-navigation/native';
+import NumericInput from 'react-native-numeric-input'
+import CheckBox from '@react-native-community/checkbox';
+import * as ImagePicker from 'react-native-image-picker';
+const includeExtra = true;
+
+//firebase stuff
+import {ScrollView} from 'react-native';
+import auth, {firebase} from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
+
+
+//form
+import {Formik} from 'formik';
+
+//import other screens
+import MerchantHeader from '../PageHeader';
+import MerchantNavigation from '../NavigationBar';
+import {styles} from '../Style';
+
+const initialState = {
+    Vegan: false,
+    Keto: false,
+    Mediterranean: false,
+    LowSugar: false,
+    LowCarb: false,
+    LowCal: false,
+  };
+
+//Main funcion
+export default function MerchantAddMenu({navigation}: {navigation: any}) {
+  const curUser = firebase.auth().currentUser;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  //checkbox
+  const [toggleButton, setToggleButton] = React.useState(false);
+  const [state, setState] = React.useState(initialState);
+  const fullpath = '/userProfile/' + 'ProfilePicture:' + curUser?.uid;
+
+  //Fetch user data on start
+  useEffect(() => {
+    if (!isLoggedIn) {
+      if (curUser) {
+        setIsLoggedIn(true);
+      } else {
+        console.log('Error in retrieving user data');
+      }
+    }
+  }, [curUser, isLoggedIn]);
+  
+
+  //Used for menu photo
+  //used for profile picture
+  const [response, setResponse] = React.useState<any>(null);
+  const onImageSelect = React.useCallback((type: any, options: any) => {
+    if (type === 'capture') {
+      ImagePicker.launchCamera(options, setResponse);
+    } else {
+      ImagePicker.launchImageLibrary(options, setResponse);
+    }
+  }, []);
+
+  //upload to firebase
+  const [pfpUri, setpfpUri] = useState(null);
+  
+
+  return (
+    <View>
+      <ScrollView>
+        {/* Banner */}
+        <MerchantHeader navigation={navigation} />
+
+        {/* Main Content */}
+      
+
+       
+        <View style={styles.container}>
+          {/* Profile */}
+          <Text style={styles.Subheading}>Menu Information</Text>
+          <>
+            <Text style={styles.Subheading}>Menu Picture:</Text>
+            {/* Image Selection */}
+            <View>
+              {actions.map(({title, type, options}) => {
+                return (
+                  <Button
+                    style={styles.button}
+                    textColor="black"
+                    mode="contained"
+                    key={title}
+                    onPress={() => {
+                      onImageSelect(type, options);
+                    }}>
+                    {title}
+                  </Button>
+                );
+              })}
+            </View>
+            {/* Show response */}
+            {response?.assets &&
+              response?.assets[0].uri &&
+              response?.assets.map(({uri}: {uri: string}) => (
+                <View style={styles.container} key={uri}>
+                  <Image
+                    resizeMode="cover"
+                    resizeMethod="scale"
+                    // eslint-disable-next-line react-native/no-inline-styles
+                    style={{
+                      height: 200,
+                      width: 200,
+                      borderColor: 'black',
+                      borderWidth: 2,
+                    }}
+                    source={{uri: uri}}
+                  />
+                  <Button
+                    style={styles.button}
+                    textColor="black"
+                    mode="contained"
+                    onPress={async () => {
+                      //assign variable the uri
+                      setpfpUri(response.assets[0].uri);
+                      console.log('file: ', pfpUri);
+                      const filename = 'ProfilePicture:' + curUser?.uid;
+                      //change uri link if needed
+                      if (pfpUri != null) {
+                        const uploadUri =
+                          Platform.OS === 'ios'
+                            ? pfpUri.replace('file://', '')
+                            : pfpUri;
+                        //check if there is a pfp already (if there is an image url)
+                        const fullpath = '/userProfile/' + filename;
+                        if (storage().ref(fullpath).getDownloadURL() != null) {
+                          storage().ref(fullpath).delete();
+                        }
+                        //put file on storage
+                        const task = storage()
+                          .ref('/userProfile/' + filename)
+                          .putFile(uploadUri);
+                        try {
+                          await task;
+                        } catch (e) {
+                          console.error(e);
+                        }
+                        //success
+                        Alert.alert(
+                          'Photo uploaded!',
+                          'Your photo has been uploaded to Firebase Cloud Storage!',
+                        );
+                        setpfpUri(null);
+                      }
+                    }}>
+                    Upload image
+                  </Button>
+                </View>
+              ))}
+          </>
+          
+          <TextInput style={styles.input} placeholder="Menu Name..." />
+          <TextInput
+            style={styles.input}
+            placeholder="Menu Description..."
+          />
+         
+
+         
+
+          {/* Details */}
+          <Formik
+            initialValues={{
+              Name: '',
+              Category: '',
+              Price: '',
+              Type: '',
+              Address: '',
+              Opening: '',
+              Closing: '',
+            }}
+            onSubmit={values => {
+              if (firestore().collection('merchant').doc(curUser?.uid)) {
+                //user data found found
+                console.log('user data found');
+                firestore()
+                  .collection('merchant')
+                  .doc(curUser?.uid)
+                  .set({
+                    Name: values.Name,
+                    Category: values.Category,
+                    Price: values.Price,
+                    Address: values.Address,
+                    Opening: values.Opening,
+                    Closing: values.Closing,
+                  })
+                  .then(() => {
+                    console.log('User updated!');
+                  });
+              }
+            }}>
+            {({handleChange, handleBlur, handleSubmit, values}) => (
+              <>
+                <Text style={styles.Subheading}>Details:</Text>
+
+                <View style={{flexDirection:'row',alignItems:'center',marginTop: 15}}>
+                    <Text style={{marginRight: 20,fontWeight:'bold'}}>Stocks</Text>
+
+                    <NumericInput type='up-down' 
+                    onChange={value => console.log(value)}
+                    totalWidth={200} 
+                    totalHeight={40} 
+                    iconSize={25}
+                    step={1}
+                    textColor='black' 
+                    valueType='integer'
+                    minValue={0}
+                    rightButtonBackgroundColor='#EA3788' 
+                    leftButtonBackgroundColor='#E56B70'
+                    rounded 
+                    iconStyle={{ color: 'black' }} 
+
+                    />
+                </View>
+                
+    
+                <TextInput
+                  style={styles.input}
+                  placeholder="Price..."
+                  value={values.Name}
+                  onChangeText={handleChange('Name')}
+                  onBlur={handleBlur('Name')}
+                />
+                
+                <SegmentedButtons
+                  style={styles.segmentButton}
+                  value={values.Type}
+                  onValueChange={handleChange('Type')}
+                  theme={buttons}
+                  buttons={[
+                    {
+                      value: 'Appetizer',
+                      label: 'Appetizer',
+                      style: {
+                        borderWidth: 0,
+                        borderRadius: 15,
+                        backgroundColor:
+                          values.Type === 'Appetizer'
+                            ? buttons.colors.primary
+                            : buttons.colors.background,
+                      },
+                    },
+                    {
+                      value: 'Main',
+                      label: 'Main',
+                      style: {
+                        borderWidth: 0,
+                        borderRadius: 15,
+                        backgroundColor:
+                          values.Type === 'Main'
+                            ? buttons.colors.primary
+                            : buttons.colors.background,
+                      },
+                    },
+                    {
+                      value: 'Dessert',
+                      label: 'Dessert',
+                      style: {
+                        borderWidth: 0,
+                        borderRadius: 15,
+                        backgroundColor:
+                          values.Type === 'Dessert'
+                            ? buttons.colors.primary
+                            : buttons.colors.background,
+                      },
+                    },
+                  ]}
+                />
+                <View>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      justifyContent: 'space-around',
+                    }}>
+                    <View style={styles.checkboxWrapper}>
+                      <CheckBox
+                        value={state.Vegan}
+                        onValueChange={value =>
+                          setState({
+                            ...state,
+                            Vegan: value,
+                          })
+                        }
+                      />
+                      <Text>Vegan</Text>
+                    </View>
+
+                    <View style={styles.checkboxWrapper}>
+                      <CheckBox
+                        value={state.Keto}
+                        onValueChange={value =>
+                          setState({
+                            ...state,
+                            Keto: value,
+                          })
+                        }
+                      />
+                      <Text>Keto</Text>
+                    </View>
+
+                    <View style={styles.checkboxWrapper}>
+                      <CheckBox
+                        value={state.Mediterranean}
+                        onValueChange={value =>
+                          setState({
+                            ...state,
+                            Mediterranean: value,
+                          })
+                        }
+                      />
+                      <Text>Mediterranean</Text>
+                    </View>
+                  </View>
+
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      justifyContent: 'space-around',
+                    }}>
+                    <View style={styles.checkboxWrapper}>
+                      <CheckBox
+                        value={state.LowSugar}
+                        onValueChange={value =>
+                          setState({
+                            ...state,
+                            LowSugar: value,
+                          })
+                        }
+                      />
+                      <Text>Low Sugar</Text>
+                    </View>
+
+                    <View style={styles.checkboxWrapper}>
+                      <CheckBox
+                        value={state.LowCarb}
+                        onValueChange={value =>
+                          setState({
+                            ...state,
+                            LowCarb: value,
+                          })
+                        }
+                      />
+                      <Text>Low Carb</Text>
+                    </View>
+
+                    <View style={styles.checkboxWrapper}>
+                      <CheckBox
+                        value={state.LowCal}
+                        onValueChange={value =>
+                          setState({
+                            ...state,
+                            LowCal: value,
+                          })
+                        }
+                      />
+                      <Text>Low Cal</Text>
+                    </View>
+                  </View>
+                </View>
+                
+                {/* Confirm Button */}
+                <Button
+                  style={styles.button}
+                  textColor="black"
+                  mode="contained"
+                  onPress={handleSubmit}>
+                  Confirm
+                </Button>
+              </>
+            )}
+          </Formik>
+        </View>
+
+        
+
+        {/* Navigation Bar */}
+        <MerchantNavigation navigation={navigation} />
+      </ScrollView>
+    </View>
+  );
+}
+
+
+
+const buttons = {
+    roundness: 5,
+    colors: {
+      primary: '#A9FDAC',
+      accent: '#f1c40f',
+      background: '#f2f2f2',
+    },
+  };
+  
+  interface Action {
+    title: string;
+    type: 'capture' | 'library';
+    options: ImagePicker.CameraOptions | ImagePicker.ImageLibraryOptions;
+  }
+  
+  const actions: Action[] = [
+    {
+      title: 'Select Image',
+      type: 'library',
+      options: {
+        selectionLimit: 1,
+        mediaType: 'photo',
+        includeBase64: false,
+        includeExtra,
+      },
+    },
+  ];
---
+++ screens/merchant/MerchantAppetizer.tsx
@@ -0,0 +1,180 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable react-native/no-inline-styles */
+import React, {useEffect, useState} from 'react';
+import 'react-native-gesture-handler';
+import {View, Image, TouchableOpacity, ScrollView} from 'react-native';
+import {StackActions} from '@react-navigation/native';
+import {styles} from '../Style';
+//material ui + form
+import {Divider, Text} from 'react-native-paper';
+import MerchantNavigation, {UserNavigation} from '../NavigationBar';
+import MerchantHeader, {UserHeader} from '../PageHeader';
+//firebase
+import firestore from '@react-native-firebase/firestore';
+
+let nextId = 0;
+interface FoodData {
+  stock: number;
+  key: number;
+  restoID: string;
+  name: string;
+  category: string;
+  image: string;
+  description: string;
+  price: number;
+  calorie: number;
+}
+
+//main
+export default function MerchantAppetizer({navigation}: {navigation: any}) {
+  const [item, setItem] = useState<FoodData[]>([]);
+
+  //retrieve restaurant information
+  useEffect(() => {
+    const fetchRestaurants = async () => {
+      const querySnapshot = await firestore()
+        .collection('fooditems')
+        .where('restoID', '==', 'nameAddress')
+        .get();
+      const fetchedRestaurants = querySnapshot.docs.map(
+        doc => doc.data() as FoodData,
+      );
+      setItem(fetchedRestaurants);
+    };
+    fetchRestaurants();
+  }, []);
+
+  //Menu Item Component
+  const Menu = (props: {
+    MenuName: string;
+    CalorieIntake: number;
+    Price: number;
+    Image: string;
+    Stock: number;
+    Navigate: string;
+  }) => {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          marginLeft: 15,
+          justifyContent: 'space-around',
+          marginTop: 10,
+        }}>
+        <View>
+          <Text>{props.MenuName}</Text>
+          <View style={{flexDirection:'row'}}>
+            <Text>Nutritional details:</Text>
+            <Text> {props.CalorieIntake} kcal</Text>
+          </View>
+          
+          <View style={{flexDirection:'row'}}>
+            <Text>Stocks left: </Text>
+            <Text>{props.Stock}</Text>
+          </View>
+
+          <Text style={styles.Highlight}>Customer Insights</Text>
+          
+        </View>
+
+        <View>
+          <Text>IDR {props.Price}</Text>
+        </View>
+
+        <View>
+          <Image
+            style={{
+              borderWidth: 1,
+              borderColor: 'black',
+              borderRadius: 8,
+              backgroundColor: '#A9FDAC',
+              width: 60,
+              height: 60,
+            }}
+            source={{uri: props.Image}}
+          />
+          <View
+            style={{
+              borderWidth: 0.5,
+              borderColor: 'black',
+              borderRadius: 50,
+              backgroundColor: 'white',
+              width: 65,
+              height: 18,
+              alignItems: 'center',
+              marginTop: 5,
+            }}>
+            <TouchableOpacity onPress={props.Navigate}>
+              {/* edit button */}
+              <Text>Edit</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
+  const RestoList = () => {
+    return item.map(element => {
+      return (
+        <View key={element.key}>
+          <Menu
+            MenuName={element.name}
+            CalorieIntake={element.calorie}
+            Price={element.price}
+            Image={element.image}
+            Stock={element.stock}
+          />
+        </View>
+      );
+    });
+  };
+
+  
+
+  return (
+    <View style={{flex: 1}}>
+      <ScrollView>
+        <View style={styles.containerUncentered}>
+          {/* Banner */}
+          <MerchantHeader navigation={navigation} />
+          {/* Content */}
+
+          <View style={{flexDirection: 'row',alignItems:'center'}}>
+            <Text
+              style={{
+                fontSize: 30,
+                marginLeft: 40,
+                marginTop: 20,
+                justifyContent: 'space-around',
+              }}>
+              Appetizer Menu
+            </Text>
+            <Text  onPress={() => {navigation.dispatch(StackActions.replace('MerchantUserView'));}}
+            style={{fontWeight: 'bold', fontSize: 16,marginTop: 20,marginLeft: 60}}>
+                User View
+            </Text>
+            <Divider style={{width: '80%', marginTop: 10}} />
+          </View>
+
+          <Menu
+            MenuName="Nasi Goreng w/ Seafood"
+            CalorieIntake={318}
+            Price={5}
+            Image="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
+            Stock={78}
+            Navigate={() => {navigation.dispatch(StackActions.replace('MerchantAddMenu'));}}
+          />
+          
+          
+
+          <View style={{marginBottom: 25}}>{RestoList()}</View>
+
+          {/* Navigation */}
+          <MerchantNavigation navigation={navigation} />
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
