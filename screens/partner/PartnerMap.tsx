5d5540d419c19b6e58f2dc11bcc58a4f69186363
NoodleReza <reza.brilliansah77@gmail.com>
2023-05-24 08:55:52 +0700

    Possible fix?

--- screens/partner/PartnerMap.tsx
+++ screens/partner/PartnerMap.tsx
@@ -1,280 +1,282 @@
-import { View, Text, SafeAreaView, StyleSheet, Image } from 'react-native'
-import React, { useState } from 'react'
-import { PartnerHeader } from '../PageHeader'
-import { styles } from '../Style';
-import { PartnerNavigation } from '../NavigationBar';
-import MapView, { Marker } from 'react-native-maps';
-import { Button, Card, IconButton, List } from 'react-native-paper';
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable react-native/no-inline-styles */
+import {View, SafeAreaView, StyleSheet, Image} from 'react-native';
+import React, {useState} from 'react';
+import {PartnerHeader} from '../PageHeader';
+import {styles} from '../Style';
+import {PartnerNavigation} from '../NavigationBar';
+import MapView, {Marker} from 'react-native-maps';
+import {Button, Card, IconButton, List} from 'react-native-paper';
 
-export default function PartnerMap ({navigation}: {navigation: any}) {
-
+export default function PartnerMap({navigation}: {navigation: any}) {
   const [state, setState] = useState<number>(1);
 
   return (
     <SafeAreaView style={styles.containerUncentered}>
-        {/* Banner */}
-        <PartnerHeader navigation={navigation} />
-        {/* Content */}
-        <View style={styles.contentBox}>
-          {/* Map */}
-          <MapView 
+      {/* Banner */}
+      <PartnerHeader navigation={navigation} />
+      {/* Content */}
+      <View style={styles.contentBox}>
+        {/* Map */}
+        <MapView
           style={MapStyles.flex}
           initialRegion={{
             latitude: -3.722,
             longitude: -38.515,
             latitudeDelta: 0.04,
             longitudeDelta: 0.05,
-          }}
-          >
-          
+          }}>
           {/* WAITING ORDER STATE */}
-          {
-            state == 1 ? 
+          {state === 1 ? (
             <>
-            <Marker
-            description="Driver Location"
-            coordinate={{
-              latitude: -3.723,
-              longitude: -38.515,
-            }}
-            >
-            <Image 
-            source={require('../../assets/driverMarker.png')} 
-            style={MapStyles.driverMarker}/>
-
-            </Marker>
-
+              <Marker
+                description="Driver Location"
+                coordinate={{
+                  latitude: -3.723,
+                  longitude: -38.515,
+                }}>
+                <Image
+                  source={require('../../assets/driverMarker.png')}
+                  style={MapStyles.driverMarker}
+                />
+              </Marker>
             </>
-          : null
-          }
-          </MapView>
-          {/* add some mechanism to automatically increment state when receiving new order ??? */}
-`         {
-            state == 1 ?
-            (<>
-            <Card mode='elevated' style={{marginBottom: 20}}>
+          ) : null}
+        </MapView>
+
+        {/* add some mechanism to automatically increment state when receiving new order ??? */}
+        {state === 1 ? (
+          <>
+            <Card mode="elevated" style={{marginBottom: 20}}>
               <Card.Content>
                 <List.Item
-                title="Waiting for order..."
-                description="Tips: You can go offline to stop receiving orders"
-                left={() =>
-                <IconButton icon="bike" size={30}/>}
+                  title="Waiting for order..."
+                  description="Tips: You can go offline to stop receiving orders"
                 />
               </Card.Content>
             </Card>
-            </>)
-            : null
-          }`
-          {/* CONFIRMING ORDER STATE */}
-         
-            {
-            state == 2 ?
-            <>
+          </>
+        ) : null}
+
+        {/* CONFIRMING ORDER STATE */}
+        {state === 2 ? (
+          <>
             <Marker
-            description="Driver Location"
-            coordinate={{
-              latitude: -3.723,
-              longitude: -38.515,
-            }}
-            >
-            <Image 
-            source={require('../../assets/driverMarker.png')} 
-            style={MapStyles.driverMarker}/>
-
-            </Marker>
-
-            <Marker
-            description="User Location"
-            coordinate={{
-              latitude: -3.719,
-              longitude: -38.511,
-            }}
-            >
-            <Image 
-            source={require('../../assets/destination.png')} 
-            style={MapStyles.pinPoint}/>
-
-            </Marker>
-
-            <Marker
-            description="Merchant Location"
-            coordinate={{
-              latitude: -3.727,
-              longitude: -38.514,
-            }}
-            >
-            <Image 
-            source={require('../../assets/restaurant.png')} 
-            style={MapStyles.pinPoint}/>
-
-            </Marker>
-
-            <Card mode='elevated' style={{marginBottom: 20}}>
-            <Card.Content>
-              <List.Item
-              title="Confirm Delivery Order"
-              description="Restaurant located 800m from you"
-              left={() =>
-              <IconButton icon="bike" size={30}/>}
-              right={() =>
-              <View>
-                <Button>Cancel</Button>
-                <Button mode="contained" style={MapStyles.button} onPress={() => setState(state + 1)}>Confirm</Button>
-              </View>
-             }
+              description="Driver Location"
+              coordinate={{
+                latitude: -3.723,
+                longitude: -38.515,
+              }}>
+              <Image
+                source={require('../../assets/driverMarker.png')}
+                style={MapStyles.driverMarker}
               />
-            </Card.Content>
-            </Card>
-            </>
-            :null
-          }
-
-          {/* WORKING ON ORDER STATE */}
-
-          {
-            state == 3 ?
-            <>
-            <Marker
-            description="Driver Location"
-            coordinate={{
-              latitude: -3.723,
-              longitude: -38.515,
-            }}
-            >
-            <Image 
-            source={require('../../assets/driverMarker.png')} 
-            style={MapStyles.driverMarker}/>
-
             </Marker>
 
             <Marker
-            description="User Location"
-            coordinate={{
-              latitude: -3.719,
-              longitude: -38.511,
-            }}
-            >
-            <Image 
-            source={require('../../assets/destination.png')} 
-            style={MapStyles.pinPoint}/>
-
-            </Marker>
-
-            <Marker
-            description="Merchant Location"
-            coordinate={{
-              latitude: -3.727,
-              longitude: -38.514,
-            }}
-            >
-            <Image 
-            source={require('../../assets/restaurant.png')} 
-            style={MapStyles.pinPoint}/>
-
-            </Marker>
-
-            <Card mode='elevated' style={{marginBottom: 20}}>
-            <Card.Content>
-              <List.Item
-              title="Restaurant Name"
-              description="Restaurant Address"
-              left={() => <Image source={require('../../assets/person.png')} style={{width: 50, height: 50,marginTop:20}}/>}
-              right={() =>
-              <View style={{justifyContent:'space-evenly'}}>
-                <Button mode="contained" style={MapStyles.button}>Message Cust.</Button>
-                <Button mode="contained" style={MapStyles.button} onPress={() => setState(state + 1)}>Finish</Button>
-              </View>
-             }
+              description="User Location"
+              coordinate={{
+                latitude: -3.719,
+                longitude: -38.511,
+              }}>
+              <Image
+                source={require('../../assets/destination.png')}
+                style={MapStyles.pinPoint}
               />
-            </Card.Content>
-            </Card>
-            </>
-            :null
-          }
-          {/* DELIVERING ORDER STATE */}
-          {
-            state == 4 ?
-            <>
-            <Marker
-            description="Driver Location"
-            coordinate={{
-              latitude: -3.723,
-              longitude: -38.515,
-            }}
-            >
-            <Image 
-            source={require('../../assets/driverMarker.png')} 
-            style={MapStyles.driverMarker}/>
-
             </Marker>
 
             <Marker
-            description="User Location"
-            coordinate={{
-              latitude: -3.719,
-              longitude: -38.511,
-            }}
-            >
-            <Image 
-            source={require('../../assets/destination.png')} 
-            style={MapStyles.pinPoint}/>
-
-            </Marker>
-
-            <Marker
-            description="Merchant Location"
-            coordinate={{
-              latitude: -3.727,
-              longitude: -38.514,
-            }}
-            >
-            <Image 
-            source={require('../../assets/restaurant.png')} 
-            style={MapStyles.pinPoint}/>
-
-            </Marker>
-
-            <Card mode='elevated' style={{marginBottom: 20}}>
-            <Card.Content>
-              <List.Item
-              title="Customer Name"
-              description="Customer Address"
-              left={() => <Image source={require('../../assets/person.png')} style={{width: 50, height: 50,marginTop:20}}/>}
-              right={() =>
-              <View style={{justifyContent:'space-evenly'}}>
-                <Button mode="contained" style={MapStyles.button}>Message</Button>
-                <Button mode="contained" style={MapStyles.button} onPress={() => setState(state - 3)}>Finish</Button>
-              </View>
-             }
+              description="Merchant Location"
+              coordinate={{
+                latitude: -3.727,
+                longitude: -38.514,
+              }}>
+              <Image
+                source={require('../../assets/restaurant.png')}
+                style={MapStyles.pinPoint}
               />
-            </Card.Content>
+            </Marker>
+
+            <Card mode="elevated" style={{marginBottom: 20}}>
+              <Card.Content>
+                <List.Item
+                  title="Confirm Delivery Order"
+                  description="Restaurant located 800m from you"
+                  left={() => <IconButton icon="bike" size={30} />}
+                  right={() => (
+                    <View>
+                      <Button>Cancel</Button>
+                      <Button
+                        mode="contained"
+                        style={MapStyles.button}
+                        onPress={() => setState(state + 1)}>
+                        Confirm
+                      </Button>
+                    </View>
+                  )}
+                />
+              </Card.Content>
             </Card>
-            </>
-            :null
-          }
-          
-        </View>
-        {/* Navigation */}
-        <PartnerNavigation navigation={navigation} />
+          </>
+        ) : null}
+
+        {/* WORKING ON ORDER STATE */}
+        {state === 3 ? (
+          <>
+            <Marker
+              description="Driver Location"
+              coordinate={{
+                latitude: -3.723,
+                longitude: -38.515,
+              }}>
+              <Image
+                source={require('../../assets/driverMarker.png')}
+                style={MapStyles.driverMarker}
+              />
+            </Marker>
+
+            <Marker
+              description="User Location"
+              coordinate={{
+                latitude: -3.719,
+                longitude: -38.511,
+              }}>
+              <Image
+                source={require('../../assets/destination.png')}
+                style={MapStyles.pinPoint}
+              />
+            </Marker>
+
+            <Marker
+              description="Merchant Location"
+              coordinate={{
+                latitude: -3.727,
+                longitude: -38.514,
+              }}>
+              <Image
+                source={require('../../assets/restaurant.png')}
+                style={MapStyles.pinPoint}
+              />
+            </Marker>
+
+            <Card mode="elevated" style={{marginBottom: 20}}>
+              <Card.Content>
+                <List.Item
+                  title="Restaurant Name"
+                  description="Restaurant Address"
+                  left={() => (
+                    <Image
+                      source={require('../../assets/person.png')}
+                      style={{width: 50, height: 50, marginTop: 20}}
+                    />
+                  )}
+                  right={() => (
+                    <View style={{justifyContent: 'space-evenly'}}>
+                      <Button mode="contained" style={MapStyles.button}>
+                        Message Cust.
+                      </Button>
+                      <Button
+                        mode="contained"
+                        style={MapStyles.button}
+                        onPress={() => setState(state + 1)}>
+                        Finish
+                      </Button>
+                    </View>
+                  )}
+                />
+              </Card.Content>
+            </Card>
+          </>
+        ) : null}
+        {/* DELIVERING ORDER STATE */}
+        {state === 4 ? (
+          <>
+            <Marker
+              description="Driver Location"
+              coordinate={{
+                latitude: -3.723,
+                longitude: -38.515,
+              }}>
+              <Image
+                source={require('../../assets/driverMarker.png')}
+                style={MapStyles.driverMarker}
+              />
+            </Marker>
+
+            <Marker
+              description="User Location"
+              coordinate={{
+                latitude: -3.719,
+                longitude: -38.511,
+              }}>
+              <Image
+                source={require('../../assets/destination.png')}
+                style={MapStyles.pinPoint}
+              />
+            </Marker>
+
+            <Marker
+              description="Merchant Location"
+              coordinate={{
+                latitude: -3.727,
+                longitude: -38.514,
+              }}>
+              <Image
+                source={require('../../assets/restaurant.png')}
+                style={MapStyles.pinPoint}
+              />
+            </Marker>
+
+            <Card mode="elevated" style={{marginBottom: 20}}>
+              <Card.Content>
+                <List.Item
+                  title="Customer Name"
+                  description="Customer Address"
+                  left={() => (
+                    <Image
+                      source={require('../../assets/person.png')}
+                      style={{width: 50, height: 50, marginTop: 20}}
+                    />
+                  )}
+                  right={() => (
+                    <View style={{justifyContent: 'space-evenly'}}>
+                      <Button mode="contained" style={MapStyles.button}>
+                        Message
+                      </Button>
+                      <Button
+                        mode="contained"
+                        style={MapStyles.button}
+                        onPress={() => setState(state - 3)}>
+                        Finish
+                      </Button>
+                    </View>
+                  )}
+                />
+              </Card.Content>
+            </Card>
+          </>
+        ) : null}
+      </View>
+      {/* Navigation */}
+      <PartnerNavigation navigation={navigation} />
     </SafeAreaView>
   );
 }
 
 const MapStyles = StyleSheet.create({
-  flex:{
-    flex:1
+  flex: {
+    flex: 1,
   },
-  driverMarker:{
+  driverMarker: {
     width: 60,
     height: 60,
   },
-  pinPoint:{
+  pinPoint: {
     width: 20,
     height: 20,
   },
   button: {
     backgroundColor: '#00BF63',
-    marginBottom: 5
-  }
+    marginBottom: 5,
+  },
 });
-
